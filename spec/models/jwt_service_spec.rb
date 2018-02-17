7f05ab27d755cb966ca0d46707019a7c8dcec88d
schiughi <schiughi@gmail.com>
2018-02-17 17:13:09 +0900

    change to instance method

--- spec/models/jwt_service_spec.rb
+++ spec/models/jwt_service_spec.rb
@@ -1,18 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe JwtService, type: :model do
-  subject { described_class }
+  subject { JwtService.new }
   let(:payload) do
     HashWithIndifferentAccess.new({ one: 'two' })
   end
   # this is dependent on 'config/secrets.yml'
   let(:token) { 'eyJhbGciOiJIUzI1NiJ9.eyJvbmUiOiJ0d28ifQ.g4BMJ85SWt6u4z3ysDFkKxqY8nxlk9l-LjFV5QCddpo' }
 
-  describe "::encode" do
+  describe "#encode" do
     it { expect(subject.encode(payload)).to eq token }
   end
 
-  describe "::decode" do
+  describe "#decode" do
     it { expect(subject.decode(token)).to eq payload }
   end
 end
