b9b23de25992abcdb67e1be12f0b433fc5269a31
schiughi <schiughi@gmail.com>
2018-02-19 00:13:54 +0900

    add omniauth and bundle update

--- Gemfile
+++ Gemfile
@@ -25,6 +25,9 @@ gem 'bcrypt', '~> 3.1.7'
 # Use Rack CORS for handling Cross-Origin Resource Sharing (CORS), making cross-origin AJAX possible
 # gem 'rack-cors'
 gem 'jwt'
+gem 'omniauth'
+gem 'omniauth-twitter'
+gem 'omniauth-slack'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- spec/models/jwt_service_spec.rb
+++ spec/models/jwt_service_spec.rb
@@ -5,14 +5,17 @@ RSpec.describe JwtService, type: :model do
   let(:payload) do
     HashWithIndifferentAccess.new({ one: 'two' })
   end
-  # this is dependent on 'config/secrets.yml'
-  let(:token) { 'eyJhbGciOiJIUzI1NiJ9.eyJvbmUiOiJ0d28ifQ.g4BMJ85SWt6u4z3ysDFkKxqY8nxlk9l-LjFV5QCddpo' }
+  let(:token) { subject.encode(payload) }
 
   describe "#encode" do
-    it { expect(subject.encode(payload)).to eq token }
+    it "should equals token" do
+      expect(subject.encode(payload)).to eq token
+    end
   end
 
   describe "#decode" do
-    it { expect(subject.decode(token)).to eq payload }
+    it "should equals payload" do
+      expect(subject.decode(token)).to eq payload
+    end
   end
 end
